d36296c7647d2b0324e6e60f0be1bc897a6bfe3d
Adam Pažičan <ado99881@gmail.com>
2018-08-01 11:51:25 +0200

    subjects controller refactored

--- api/subjects-controller.js
+++ api/subjects-controller.js
@@ -1,43 +1,59 @@
 const Subject = require('../models/subject')
 
-function getSubjects(req, res){
-    Subject.find().then(result => res.json(result))
-    //Subject.find((err, result) => res.json(result))
+async function getSubjects(req, res){
+    try{
+        const subjects = await Subject.find()
+        res.json(subjects)
+    }
+    catch(err){
+        res.status(400).json({ status: '400', message: 'Couldn\'t get articles'})
+    }
 }
 
-function createSubject(req, res){
-    const name = req.body.name
-    const image = req.body.image
+async function createSubject(req, res){
+    const { name, image } = req.body
 
     const subject = new Subject({
         name,
         image
     })
 
-    subject.save().then(newDoc =>
+    try{
+        const newDoc = await subject.save()
         res.json({ status: "ok", ...newDoc._doc})    
-    )
+    }
+    catch(err){
+        res.status(400).json({ status: '400', message: 'Couldn\'t create articles'})
+    }
 }
 
-function updateSubject(req, res){
+async function updateSubject(req, res){
     const _id = req.params.subjectId
-    const name = req.body.name
-    const image = req.body.image
+    const { name, image } = req.body
 
-    Subject.findOne({ _id })
-        .then(doc => {
-            doc.name = name || doc.name
-            doc.image = image || doc.image
-            doc.save().then(newDoc => 
-                res.json({ status: "ok", ...newDoc._doc })
-            )
-        })
+    try{
+        const subject = await Subject.findOne({ _id })
+        subject.name = name || subject.name
+        subject.image = image || subject.image
+        
+        const newDoc = await subject.save()
+        res.json({ status: "ok", ...newDoc._doc })
+    }
+    catch(err){
+        res.status(400).json({ status: '400', message: 'Couldn\'t update articles'})
+    }
 }
 
-function removeSubject(req, res){
+async function removeSubject(req, res){
     const _id = req.params.subjectId
     
-    Subject.remove({ _id }).then(_ => res.json({ status: "ok" }))
+    try{
+        await Subject.remove({ _id })
+        res.json({ status: "ok" })
+    }
+    catch(err){
+        res.status(400).json({ status: '400', message: 'Couldn\'t remove articles'})
+    }
 }
 
 
